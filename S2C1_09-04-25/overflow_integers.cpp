d09f231561ba6fa4f85389cb1ffdf5109d389955
Bryan_Anaya <banaya@unal.edu.co>
2025-04-09 11:44:55 -0500

    Add overflow

---
+++ S2C1_09-04-25/overflow_integers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+
+// declaración
+//void printNumbers();
+
+int main(void)
+{
+    int a = 1;
+    //bool in =  true;
+    while (a > 0)
+    {   
+        a = a*2;
+        std::cout << a << std::endl;
+    }
+    
+    return 0;
+
+    // TODO
+    
+}
