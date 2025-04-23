5a0065f3779fe73984de1fe4d514566a71839af2
Bryan_Anaya <banaya@unal.edu.co>
2025-04-23 12:49:27 -0500

    Modularized versions and editted makefile using some internals vars

--- S3_C1_04-23-25_Makefiles/sumupdown.cpp
+++ S3_C1_04-23-25_Makefiles/sumupdown.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <cmath>
 
-typedef float REAL; 
-
-REAL sumup(int nterms);
-REAL sumdown(int nterms);
+#include "functions.h"
 
 int main(int argc, char **argv)
 {
@@ -19,21 +16,3 @@ int main(int argc, char **argv)
   return 0;
 }
 
-REAL sumup(int nterms)
-{
-    // todo
-    REAL result = 0.0;
-    for (int n = 1; n <= nterms; n++) {
-        result += 1.0/n;
-    }
-    return result;
-}
-
-REAL sumdown(int nterms)
-{
-    REAL result = 0.0;
-    for (int n = nterms; n >= 1; n--) {
-        result += 1.0/n;
-    }
-    return result;
-}
