1b9b285f40bf54fd0d559179935333f59503b0de
Bryan_Anaya <banaya@unal.edu.co>
2025-05-13 23:48:02 -0500

    Running sanitizers ok

--- workshops/S6_debugging/code.cpp
+++ workshops/S6_debugging/code.cpp
@@ -12,16 +12,17 @@ int main(int argc, char **argv)
     int ii, jj;
     ii = 0;
     jj = -1;
+
+    // Se llaman estas funciones pero no se usan en alguna otra parte del código
     foo(ii, jj);
     foo(jj, ii);
-
     baz(25.9);
 
     const int NX = 2, NY = 3, NZ = 4;
     double *x, y[NY], z[NZ];
     x = new double[NX];
 
-    //int ii, jj, kk; No se deben re declarar variables, además a la variable kk no se le da uso
+    //int ii, jj, kk; No se deben re declarar variables, además la variable kk no se le da uso
 
     print_array(x, NX);
     print_array(y, NY);
@@ -30,41 +31,49 @@ int main(int argc, char **argv)
 
     for (ii = 0; ii < NX; ++ii)
     {
-        x[jj] = ii;
+        x[ii] = jj;
     }
-    print_array(x, NZ);
+    print_array(x, NX);
     print_array(y, NY);
-    print_array(z, NZ + NY);
+    print_array(z, NZ);
     std::cout << std::endl;
     for (jj = 0; jj < NY; ++jj)
     {
-        x[jj] = ii;
+        if(jj < NX){
+            x[jj] = ii;
+        }
         y[jj] = jj;
     }
-    print_array(x - NY, NY);
-    print_array(y - NZ, NZ);
-    print_array(z + NZ + NY, NX);
+    //print_array(x - NY, NY);
+    //print_array(y - NZ, NZ);
+    //print_array(z + NZ + NY, NX);
     std::cout << std::endl;
 
+    delete[] x;
     return EXIT_SUCCESS;
 }
 
 int foo(int a, int b)
 {
+    // Evitar divisiones por cero
+    if (b == 0 || a == 0 || bar(a, b) == 0){
+        std::cout << "Error: división por cero." << std::endl;
+        return 0;
+    }
     return a / b + b / bar(a, b) + b / a;
 }
 
 int bar(int a, int b)
 {
-    unsigned int c = a;
+    int c = std::abs(a);
     return c + a - b;
 }
 
 double baz(double x)
 {
-    if (x = 0)
-        return 0; // se debe retornal algo
-    double v = 1 - (x + 1);
+    if (x == 0){
+        return 0;
+    }
     return std::sqrt(x);
 }
 
